c1c8f072edad0fa3c3af0b9ae914aed63af745d4
Moises <MoisesTabar.013@gmail.com>
2021-03-13 15:56:36 -0400

    v1 version of the API Finished

--- src/database/ioredis.ts
+++ src/database/ioredis.ts
@@ -1,8 +1,8 @@
 import Redis from 'ioredis';
 
-export function redisConnection(uri: string){
+export async function redisConnection(uri: string){
     try{
-        const redis = new Redis(uri);
+        const redis = await new Redis(uri);
         const connectionStatus = redis.status;
         if(connectionStatus == 'connecting'){
             console.log(`Redis connection established correctly: ${connectionStatus}`);
